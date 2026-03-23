73c3e9b59b331c8ad9a6a0f028f94dabe496a2ce
moyshi <moyshi.deutsch@gmail.com>
2026-03-23 12:59:34 +0200

    add dialog and fix state memo problems

--- app/_components/TicTacToe.tsx
+++ app/_components/TicTacToe.tsx
@@ -1,48 +1,50 @@
-import { useState } from "react";
-import { Button } from "@mui/material";
+import { useCallback, useMemo, useState } from "react";
+import { Button, Dialog } from "@mui/material";
 
 const OPTIONS_TO_FILL = ['X', 'O']
 const BOARD_LENGTH = 3
 
 export default function TicTacToe({firstPlayerName, secondPlayerName}: {firstPlayerName:string, secondPlayerName:string}) {
-  const [board, setBoard] = useState<string[][]>(Array(BOARD_LENGTH).fill([]).map(()=>Array(BOARD_LENGTH).fill('')))
+  const [board, setBoard] = useState<string[][]>(Array.from({ length: BOARD_LENGTH }, () => Array.from({length: BOARD_LENGTH}, () => ('')) ) )
   const [turnNum, setTurnNum] = useState<number>(0)
-  const boardClick = (y:number, x:number) => {
+  const boardClick = useCallback((y:number, x:number) => {
     setBoard((priv) => {
       if (priv[y][x]) {return priv};
-      priv[y][x] = OPTIONS_TO_FILL[turnNum % 2]
-      setTurnNum(turnNum+1)
-      return [...priv]
+      priv = [
+        ...priv.slice(0, y),
+        priv[y].map((v, i) => (i === x) ? OPTIONS_TO_FILL[turnNum % 2] : v),
+        ...priv.slice(y + 1)
+    ]
+      setTurnNum(turnNum + 1)
+      return priv
     })
-  }
-  const winner = checkWin(board)
-  if (winner) {
-    return (
-      <>
-        <h1>{(OPTIONS_TO_FILL.indexOf(winner) === 0) ? firstPlayerName : secondPlayerName} wins!</h1>
-        <Button variant="outlined" onClick={() => window.location.reload()}>
-          new game
-        </Button>
-      </>
-    )
-  }
-  if (turnNum == BOARD_LENGTH**2) {
-    return (
-      <>
-        <h1>game over, no one wins.</h1>
-        <Button variant="outlined" onClick={() => window.location.reload()}>
-          new game
-        </Button>
-      </>
-    )
-  }
+  }, [turnNum])
+  const winner = useMemo(() => checkWin(board), [board]);
   return (
-    <>
-      <h1 className="mb-10">{(turnNum % 2 === 0) ? firstPlayerName : secondPlayerName} turns</h1>
-      <Board
-        board={board}
-        click={boardClick}
-      />
+    <> 
+        <Dialog
+            open={Boolean(turnNum == BOARD_LENGTH**2 || winner)}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <div className="h-30 w-60 flex flex-col space-between justify-around items-center">
+                <h1>
+                    {Boolean(winner) ?
+                        `${(OPTIONS_TO_FILL.indexOf(winner) === 0) ? firstPlayerName : secondPlayerName} wins!`
+                    :
+                        "game over, no one wins."
+                    }
+                </h1>
+                <Button variant="outlined" onClick={() => window.location.reload()}>
+                    new game
+                </Button>
+            </div>
+        </Dialog>
+        <h1 className="mb-10">{(turnNum % 2 === 0) ? firstPlayerName : secondPlayerName} turns</h1>
+        <Board
+            board={board}
+            click={boardClick}
+        />
     </>
   )
 }
@@ -55,9 +57,9 @@ function Board({board, click}:{board:string[][], click:(y:number, x:number) => v
       <div className={`absolute bg-green-300 h-full w-2`} style={{left:`${(1 / BOARD_LENGTH) * 100}%`}}/>
       <div className={`absolute bg-green-300 h-full w-2`} style={{left:`${(2 / BOARD_LENGTH) * 100}%`}}/>
       {board.map((row, hight) => row.map((value, witdh) => 
-        <div
+        <button
           key={`${hight}${witdh}`}
-          className={`absolute text-9xl flex justify-center items-center`}
+          className={`absolute text-9xl flex justify-center items-center cursor-pointer`}
           style={{
             height:`${(1 / BOARD_LENGTH) * 100}%`,
             width:`${(1 / BOARD_LENGTH) * 100}%`,
@@ -65,7 +67,7 @@ function Board({board, click}:{board:string[][], click:(y:number, x:number) => v
             left:`${(witdh / BOARD_LENGTH) * 100}%`
           }}
           onClick={() => {click(hight, witdh)}}
-        >{value}</div>
+        >{value}</button>
       ))}
     </div>
   )
