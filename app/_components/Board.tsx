f75298d0d95eaa4ac9e957cccf0baaafa4d63685
moyshi <moyshi.deutsch@gmail.com>
2026-03-24 16:00:21 +0200

    make button disabled when mouse enter clicked, and fix slant line

--- app/_components/Board.tsx
+++ app/_components/Board.tsx
@@ -1,7 +1,13 @@
 import { Button } from "@mui/material"
 import { WinOrientation, WinResult } from "./checkWin"
+import { useCallback, useState } from "react"
 
 export default function Board({board, click, win}:{board:string[][], click:(y:number, x:number) => void, win: WinResult | void}) {
+  const [buttonsDisabled, setButtonsDisabled] = useState<Record<string, boolean>>({})
+  const changeButtonsDisabled = useCallback((key:string, status:boolean) => {
+    setButtonsDisabled((privButtonsDisabled) => {return {...privButtonsDisabled, [key]: status}})
+  }, [])
+
   return (
     <div className="flex max-h-7/10 h-[70vw] max-w-7/10 w-[70vh]  relative">
       <div className={`absolute bg-green-300 w-full h-2`} style={{top:`${(1 / board.length) * 100}%`}}/>
@@ -15,25 +21,33 @@ export default function Board({board, click, win}:{board:string[][], click:(y:nu
           gridTemplateRows: `${100/board[0].length}% `.repeat(board[0].length)
         }}
       >
-        {board.map((row, hight) => row.map((value, witdh) => 
-          <Button
-            key={`${hight}${witdh}`}
-            className={`justify-center items-center bg-red-100 h-full w-full`}
-            disabled={Boolean(value)}
-            variant="text"
-            onClick={() => {click(hight, witdh)}}
-          >
-            <p className="text-9xl">{value}</p>
-          </Button>
-        ))}
+        {board.map((row, hight) => row.map((value, witdh) => {
+          const key = `${hight}${witdh}`
+          return (
+            <Button
+              key={key}
+              className={`justify-center items-center bg-red-100 h-full w-full`}
+              sx={{ '&.MuiButton-root:hover': buttonsDisabled[key] ? {bgcolor: 'transparent', cursor: 'auto'}: {} }}
+              disabled={Boolean(value)}
+              variant="text"
+              onClick={() => {click(hight, witdh)}}
+              onMouseEnter={(e) => {if (e.buttons === 1) changeButtonsDisabled(key, true)}}
+              onMouseUp={() => changeButtonsDisabled(key, false)}
+              onMouseOut={() => changeButtonsDisabled(key, false)}
+            >
+              <p className="text-9xl">{value}</p>
+            </Button>
+          )
+        }))}
       </div>
       {(win !== undefined) &&
         <div 
-          className={`absolute bg-gray-500 h-2 origin-[13%_50%]`}
+          className={`absolute bg-gray-500 h-2`}
           style={{
             left:`${((win.x + 0.15) / board.length)  * 100}%`,
             top:`${((win.y + 0.5) / board.length) * 100}%`,
             transform: `rotate(${(win.orientation * 45)}deg)`,
+            transformOrigin: WinOrientation[win.orientation].startsWith('Slant') ? "10.3% 50%" : "13% 50%",
             width: WinOrientation[win.orientation].startsWith('Slant') ? '120%' : '90%'
           }}
         />
